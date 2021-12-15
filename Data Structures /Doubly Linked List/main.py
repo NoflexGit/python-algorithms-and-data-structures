ad11aaa4ac6fb82a7e433cdd043c4651f9d4a008
NoflexGit <untroman@mail.ru>
2021-12-15 20:11:42 +0300

    Doubly Linked List. Prepend

--- Data Structures /Doubly Linked List/main.py
+++ Data Structures /Doubly Linked List/main.py
@@ -38,6 +38,18 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -47,4 +59,5 @@ class DoublyLinkedList:
 
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
+my_doubly_linked_list.pop()
 print(my_doubly_linked_list.print_list())
